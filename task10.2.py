bcfb2e3cf92aa02f2ec913cf04caec75cd15dee7
andreibaden <andreibaden@gmail.com>
2023-04-02 11:01:42 +0300

    task 10.2 v2.0

--- task10.2.py
+++ task10.2.py
@@ -1,6 +1,6 @@
 # Task10.2 "Same numbers"
 
-# Version: 1.0
+# Version: 2.0
 # Author: Nedaboi Andrei_QA2023
 # Date: 21.03.2023
 
@@ -9,20 +9,18 @@ def same_num(num):
         return "Error! Input your number >= 10!"
 
     while num > 9:
-        flag = True
         last_digit = num % 10
         last2_digit = num % 100 // 10
         if last_digit != last2_digit:
-            flag = False
-            break
+            return False
         num //= 10
-    return flag
+    return True
 
 
 def main():
     number = int(input("Input your number >= 10: "))
     msg = "Error! Input your number >= 10!" if same_num(number) == \
-        "Error! Input your number >= 10!" else ("YES" if "YES" else "NO")
+    "Error! Input your number >= 10!" else ("YES" if same_num(number) == True else "NO")
     print(msg)
 
 
@@ -30,8 +28,8 @@ main()
 
 
 def testing():
-    result = " 222 --> YES _" + str(same_num(222) == "YES")
-    result += "\n 345 --> NO _" + str(same_num(345) == "NO")
+    result = " 222 --> True _" + str(same_num(222) == True)
+    result += "\n 345 --> False _" + str(same_num(345) == False)
     result += "\n 9 --> Error! Input your number >= 10!  _" + \
               str(same_num(9) == "Error! Input your number >= 10!")
     result += "\n -89 --> Error! Input your number >= 10!  _" + \
